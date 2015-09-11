a7b9e762ced79a0b8f24e50960d0f4c015644349
andradellos <andradellos@hotmail.com>
2015-09-11 13:42:55 -0300

    Teste 6

---
+++ TesteGitBase/TestProjectGitBase/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TesteGitBase;
+
+namespace TestProjectGitBase
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void valor_nao_pode_ser_menor_que_5()
+        {
+            Class1 CSoma = new Class1();           
+           
+            if(CSoma.Soma() < 5){
+
+                Assert.Fail("Valor pode ser menor que 5!");
+            }
+        }
+    }
+}
---
+++ TesteGitBase/TesteGitBase/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace TesteGitBase
+{
+    public class Class1
+    {
+        private int valorUm()
+        {
+            return 2;        
+        }
+        private int valorDois()
+        {
+            return 3;
+        }
+        public int Soma()
+        {            
+            return (valorUm() + valorDois());
+        }
+    }
+
+}
