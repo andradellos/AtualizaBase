239318a94b1a95a3a92df0f82ce08270f44d0a06
andradellos <andradellos@hotmail.com>
2015-09-11 15:40:54 -0300

    14

--- TesteGitBase/TesteGitBase/Default.aspx.cs
+++ TesteGitBase/TesteGitBase/Default.aspx.cs
@@ -11,7 +11,7 @@ namespace TesteGitBase
         protected void Page_Load(object sender, EventArgs e)
         {
             string dado = "C";
-            dado = "A";
+            dado = "A"
         }
     }
 }
